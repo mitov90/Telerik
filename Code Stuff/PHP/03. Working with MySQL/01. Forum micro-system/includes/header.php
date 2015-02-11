4c21e160a13a903ec2e56440299644ba71d3b921
Zhiko Mitov <mitov90@gmail.com>
2015-02-11 19:45:20 +0200

    Update

--- Code Stuff/PHP/03. Working with MySQL/01. Forum micro-system/includes/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $title ?></title>
-    <meta charset="UTF-8">
-    <link type="text/css" rel="stylesheet" href="includes/styles.css" />
-</head>
-<body>
-
